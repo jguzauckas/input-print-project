ef532f36db6c22e9f5047f18704467e25ffa527a
jguzauckas <75325154+jguzauckas@users.noreply.github.com>
2022-09-16 07:20:39 -0400

    Updated with 'not' statements

--- booleanpractice.py
+++ booleanpractice.py
@@ -1,7 +1,7 @@
 #Concrete statements are either True or False for a person, making them ideal for working with booleans.
 #Use print and input statements to have the user answer True or False to statements you provide.
 #Using their responses, utilize 'and' and 'or' to make compound statements and
-#determine if they are True or False.
+#determine if they are True or False. Use 'not' to discuss the negation of statements
 #Here is an example:
 st1 = "I am a teacher"
 st2 = "I am a high school student"
@@ -11,6 +11,7 @@ print(st2)
 ans2 = bool(input("Was this statement True or False for you: "))
 print(st1, "and", st2, "is", ans1 and ans2)
 print(st1, "or", st2, "is", ans1 or ans2)
+print("The negation of", st1, "is", not ans1)
 
 #Write 4 statements as strings and save them using variables st1, st2, st3, and st4:
 
@@ -20,3 +21,6 @@ print(st1, "or", st2, "is", ans1 or ans2)
 
 #Once you've collected answers, write four different compound statements (different combinations
 #of and/or, and st1/st2/st3/st4) and their calculated boolean values (similar to lines 12 and 13)
+
+
+#Write two different 'not' statements with their calculated boolean values (like line 14)
