a2d8622438f7032b0a77154e6b4408d2cb148f7d
jguzauckas <75325154+jguzauckas@users.noreply.github.com>
2022-09-16 09:33:07 -0400

    Added number solution files

---
+++ numberpracticesolutions.py
@@ -0,0 +1,78 @@
+#This file will have you practice integers and floats and operations on them.
+#The context: Two people both have a bank account that this program will track.
+#First, initialize two float variables, called "per1" and "per2" with two different amounts
+#between 5k and 10k dollars by asking the user to enter float values. Print a statement
+#regarding their initial balances: i.e. "Person 1 has ____ dollars and Person 2 has _____."
+print("Each person should have a starting balance between 5k and 10k.")
+per1 = float(input("Enter person 1's starting balance: "))
+per2 = float(input("Enter person 2's starting balance: "))
+print("Person 1 has", per1, "dollars and Person 2 has", per2, "dollars.")
+
+#It's Friday and for both people, their paychecks were deposited in their accounts.
+#Person 1 received 1,422.76 and person 2 received 1,546.98.
+#Use compound assignment statements to increase their bank balance appropriately and make a
+#statement regarding their new balance:
+#i.e. "After pay day, person 1 has ___ dollars and person 2 has ___ dollars"
+per1 += 1422.76
+per2 += 1546.98
+print("After pay day, Person 1 has", per1, "dollars and Person 2 has", per2, "dollars.")
+
+#On Saturday, both people run some errands and spend some money.
+#Person 1 spent 324.59 and person 2 spent 287.68.
+#Use compound assignment statements to decrease their bank balance appropriately and make a
+#statement regarding their new balance:
+#i.e. "After errands, person 1 has ___ dollars and person 2 has ___ dollars"
+per1 -= 324.59
+per2 -= 287.68
+print("After errands, Person 1 has", per1, "dollars and Person 2 has", per2, "dollars.")
+
+#On Sunday, both people went to a local fair and entered in a raffle.
+#Person 1 AND person 2 managed to win a prize that triples their money, 
+#Use compound assignment statements to modify their bank balance
+#appropriately and make a statement regarding their new balance:
+#i.e. "After the fair, person 1 has ___ dollars and person 2 has ___ dollars"
+per1 *= 3
+per2 *= 3
+print("After the fair, Person 1 has", per1, "dollars and Person 2 has", per2, "dollars.")
+
+#Over the next couple weeks, both people spent half of their money on various things.
+#Use compound assignment statements to modify their bank balance appropriately and
+#make a statement regarding their new balance:
+#i.e. "After a couple weeks, person 1 has ___ dollars and person 2 has ___ dollars"
+per1 /= 2
+per2 /= 2
+print("After a couple weeks, Person 1 has", per1, "dollars and Person 2 has", per2, "dollars.")
+
+#Due to a banking error, both people's account values were divided by 1.5 and rounded down
+#to the nearest whole dollar. Use compound assignment statements to modify their bank
+#balance appropriately and make a statement regarding their new balance:
+#i.e. "After the error, person 1 has ___ dollars and person 2 has ___ dollars"
+per1 //= 1.5
+per2 //= 1.5
+print("After the error, Person 1 has", per1, "dollars and Person 2 has", per2, "dollars.")
+
+#Both people left their accounts behind and went to another bank after the error,
+#however they left their money behind. The money sat there for 50 years. Below I have created an
+#interest rate for each person's account, titled "rate1" and "rate2"
+rate1 = 1.025
+rate2 = 1.02
+
+#To calculate the new bank balance, you need to multiply the current bank account balance value
+#(per1 and per2) by the interest rate (rate1 and rate2) raised to the power of the number of years.
+#Note: This will use multiplication assignment with the rate raised to the power in parentheses
+#(ex. var1 *= (var2 ** 50)) Modify their account balances appropriately and make a statement
+#regarding their new balance: i.e. "After 50 years, person 1 has ___ dollars and person 2 has ___ dollars"
+per1 *= (rate1 ** 50)
+per2 *= (rate2 ** 50)
+print("After 50 years, Person 1 has", per1, "dollars and Person 2 has", per2, "dollars.")
+
+#Shortly after this interest is calculated and applied, both people write a will to determine what
+#to do with their estate after they pass away. Person 1 wants to split their money between 5 different
+#charities, and Person 2 wants to split their money between 10 different charities. The problem is,
+#both only want to make whole number contributions to the charities, and leave any extra dollars and cents
+#behind. After distributing their money, what will their final account balances be?
+#Modify their account balances appropriately and make a statement regarding their new balance:
+#i.e. "After they distribute their money, person 1 has ___ dollars and person 2 has ___ dollars"
+per1 %= 5
+per2 %= 10
+print("After they distribute the money, Person 1 has", per1, "dollars and Person 2 has", per2, "dollars.")
