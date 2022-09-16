2851e8e15394c71f67001a37e8f82252f032b0b7
jguzauckas <75325154+jguzauckas@users.noreply.github.com>
2022-09-16 09:03:18 -0400

    Added stringpractice.py

---
+++ stringpractice.py
@@ -0,0 +1,35 @@
+#Prompt the user enter their full name (first and last) and save that to a variable called "name"
+
+
+#Some users have a surname (for example Junior, Senior, II, III, etc.) that they forgot to enter
+#Use compound assignment and input to ask the user for a surname and attach it to the end of the
+#name variable so it correctly holds their first, last and surname.
+
+
+#Report back to the user how many characters are in their full name using length and print.
+
+
+#As a terrible attempt at a multifactor authentication, a program asks for the 5th character of
+#the person's name (could even be a space). Report to the user which character is the 5th in their
+#name.
+#String indexing
+
+
+#It also asks for the last character in the person's name. Report to the user which is their last.
+#String indexing
+
+
+#A stock username asks that it starts with the first four letters of their name. Report to the user
+#what the first three characters of their name are.
+#String slicing without start, with end
+
+
+#The person wants to know what their name would look like if it was shorter. They decide the easiest
+#way to do this is to take off characters at the beginning and end of their name. Report to the user
+#what their name would be without the first 3 characters and without the last 2 characters.
+#String slicing with start and end
+
+
+#Another terrible authentication asks for the last 4 characters of the user's name. Report to the user
+#what the last four characters of their name is.
+#String slicing with start, without end
